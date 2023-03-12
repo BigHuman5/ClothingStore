9c035b0a0af3ecd6e33951c0822d18db99a4ba1e
Human <52690624+BigHuman5@users.noreply.github.com>
2023-03-12 18:09:51 +0600

    Добавьте файлы проекта.

---
+++ ClothingStore.DAL/EF/ClothingStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ClothingStore.DAL.EF
+{
+    public class ClothingStoreDbContext : DbContext
+    {
+        public ClothingStoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+        }
+    }
+}
---
+++ ClothingStore/Program.cs
@@ -0,0 +1,34 @@
+using ClothingStore.DAL.EF;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ClothingStoreDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
